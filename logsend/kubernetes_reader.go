c89b866bf621fd4845abac3d4feedadddbdb8641
dimafujitsu <dimitry.vasilec@est.fujitsu.com>
2015-04-01 15:37:34 +0200

    add json support

--- logsend/kubernetes_reader.go
+++ logsend/kubernetes_reader.go
@@ -3,11 +3,69 @@ package logsend
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/client/clientcmd"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/kubectl/cmd"
 	"github.com/spf13/cobra"
 	"time"
 )
+
+type Record struct {
+	Kind              string      `json:"kind"`
+	Creationtimestamp interface{} `json:"creationTimestamp"`
+	Resourceversion   int         `json:"resourceVersion"`
+	Apiversion        string      `json:"apiVersion"`
+	Items             []struct {
+		Kind              string `json:"kind"`
+		ID                string `json:"id"`
+		UID               string `json:"uid"`
+		Creationtimestamp string `json:"creationTimestamp"`
+		Selflink          string `json:"selfLink"`
+		Resourceversion   int    `json:"resourceVersion"`
+		Apiversion        string `json:"apiVersion"`
+		Namespace         string `json:"namespace"`
+		Generatename      string `json:"generateName"`
+		Labels            struct {
+			RunContainer string `json:"run-container"`
+		} `json:"labels"`
+		Desiredstate struct {
+			Manifest struct {
+				Version    string      `json:"version"`
+				ID         string      `json:"id"`
+				Volumes    interface{} `json:"volumes"`
+				Containers []struct {
+					Name  string `json:"name"`
+					Image string `json:"image"`
+					Ports []struct {
+						Containerport int    `json:"containerPort"`
+						Protocol      string `json:"protocol"`
+					} `json:"ports"`
+					Resources              struct{} `json:"resources"`
+					Terminationmessagepath string   `json:"terminationMessagePath"`
+					Imagepullpolicy        string   `json:"imagePullPolicy"`
+					Capabilities           struct{} `json:"capabilities"`
+				} `json:"containers"`
+				Restartpolicy struct {
+					Always struct{} `json:"always"`
+				} `json:"restartPolicy"`
+				Dnspolicy string `json:"dnsPolicy"`
+			} `json:"manifest"`
+			Host string `json:"host"`
+		} `json:"desiredState"`
+		Currentstate struct {
+			Manifest struct {
+				Version       string      `json:"version"`
+				ID            string      `json:"id"`
+				Volumes       interface{} `json:"volumes"`
+				Containers    interface{} `json:"containers"`
+				Restartpolicy struct{}    `json:"restartPolicy"`
+			} `json:"manifest"`
+			Status string `json:"status"`
+			Host   string `json:"host"`
+		} `json:"currentState"`
+	} `json:"items"`
+}
 
 // KubernetesReader read data from kubernetes api each 3 seconds
 func KubernetesReader() {
@@ -25,6 +83,7 @@ func KubernetesReader() {
 	buf := bytes.NewBuffer([]byte{})
 	cmd := f.NewCmdGet(buf)
 	cmd.SetOutput(buf)
+	cmd.Flags().Set("output", "json")
 	rules := RuleLoad()
 	for {
 		time.Sleep(time.Millisecond * 1000)
@@ -35,12 +94,12 @@ func KubernetesReader() {
 func getData(cmd *cobra.Command, buf *bytes.Buffer, rules []*Rule) {
 	cmd.Run(cmd, []string{"pods"})
 	reader := bufio.NewReader(buf)
-	for {
-		line, err := reader.ReadString('\n')
-
-		if err != nil {
-			break
-		}
-		checkLineRules(&line, rules)
+	x := new(Record)
+	err := json.NewDecoder(reader).Decode(x)
+	if err != nil {
+		fmt.Printf("Error json: %+v\n", err)
+	} else {
+		fmt.Printf("JSON: %+v\n", x)
+		Sender.Send(x)
 	}
 }
--- logsend/restapi_sender.go
+++ logsend/restapi_sender.go
@@ -1,12 +1,12 @@
 package logsend
 
 import (
-/*
-	"flag"
-	"strings"
-	"time"
-*/
-)
+	"github.com/jmcvetta/napping"
+	/*
+		"flag"
+		"strings"
+		"time"
+	*/)
 
 var (
 	restapiCh = make(chan *[]string, 0)
@@ -20,12 +20,31 @@ func InitRestapi(conf interface{}) {
 	go func() {
 		for data := range restapiCh {
 			debug("write by api", data)
+
+			type Foo struct {
+				Bar string
+			}
+			type Spam struct {
+				Eggs int
+			}
+			payload := Foo{
+				Bar: "baz",
+			}
+			result := Spam{}
+			url := "http://foo.com/bar"
+			resp, err := napping.Post(url, &payload, &result, nil)
+			if err != nil {
+				panic(err)
+			}
+			if resp.Status() == 200 {
+				println(result.Eggs)
+			}
 		}
 	}()
 	return
 }
 
-func (self *RestapiSender) Name() string {
+func (sender *RestapiSender) Name() string {
 	return "restapi"
 }
 
@@ -43,10 +62,10 @@ func NewRestapiSender() Sender {
 	return Sender(sender)
 }
 
-func (self *RestapiSender) Send(data interface{}) {
+func (sender *RestapiSender) Send(data interface{}) {
 	restapiCh <- data.(*[]string)
 }
 
-func (self *RestapiSender) SetConfig(rawConfig interface{}) error {
+func (sender *RestapiSender) SetConfig(rawConfig interface{}) error {
 	return nil
 }
